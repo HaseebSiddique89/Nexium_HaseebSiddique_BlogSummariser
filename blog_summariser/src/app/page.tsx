ba73544b04cdb7ca1dad788fa7d99c0fb2529d29
Haseeb Siddique <haseebsiddique825@gmail.com>
2025-07-24 18:06:01 +0500

    Update page.tsx

--- blog_summariser/src/app/page.tsx
+++ blog_summariser/src/app/page.tsx
@@ -1169,7 +1169,7 @@ export default function EnhancedBlogSummarizer() {
           </div>
           
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-sm text-gray-400">
-            <p>&copy; 2024 BlogSummarizer. All rights reserved.</p>
+            <p>&copy; 2025 BlogSummarizer. All rights reserved.</p>
           </div>
         </div>
       </footer>
